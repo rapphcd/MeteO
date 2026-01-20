1ba0334134210eb87f9b27847c7eb1f5daed6bcd
raphhcd <77208630+rapphcd@users.noreply.github.com>
2026-01-20 17:17:30 +0100

    add(forecast): ajout de la requete sur l'api forecast | edit(css): edit du renvoi de la couleur et creation d'une fonction a part

--- app.py
+++ app.py
@@ -1,20 +1,47 @@
 from flask import Flask, render_template, request, redirect, url_for
 import requests
 
-appid = "" #votre app id openweathermap
-ip = "" #votre ip
+appid = ""  # votre app id openweathermap
+ip = ""  # votre ip
 
 app = Flask(__name__)
 
+
+def getTempColor(temp):
+    if temp >= 25:
+        c = "red"
+    elif temp >= 20:
+        c = "orange"
+    elif temp <= 10:
+        c = "aqua"
+    else:
+        c = "white"
+    return c
 
 def recup_data(ville):
     url = f"https://api.openweathermap.org/data/2.5/weather?q={ville}&units=metric&appid={appid}"
     response = requests.get(url)
     dico = response.json()
-    if dico["cod"] != '404':
+    if dico["cod"] == 200:
+
         dc = {"cod": dico["cod"], "ville": dico["name"], "temp": dico["main"]["temp"],
               "humidity": dico["main"]["humidity"], "description": dico["weather"][0]["description"],
-              "icon": dico["weather"][0]["icon"]}
+              "icon": dico["weather"][0]["icon"], "country": dico["sys"]["country"], "color": getTempColor(dico["main"]["temp"])}
+
+        lon = dico['coord']['lon']
+        lat = dico['coord']['lat']
+
+        forecast_url = f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={appid}'
+
+        forecast_response = requests.get(forecast_url)
+        forecasts = forecast_response.json()
+
+        if forecasts["cod"] == "200":
+            forecast = forecasts['list'][-1]
+            final_forecast = {'temp': {'min': forecast["main"]['temp_min'], 'max': forecast["main"]['temp_max'],
+                                       'predicted': forecast["main"]['temp']}, "humidity": forecast["main"]['humidity'],
+                              "description": forecast["weather"][0]["description"], "icon": forecast["weather"][0]["icon"], "color": getTempColor(forecast["main"]['temp'])}
+            dc["forecast"] = final_forecast
     else:
         dc = {"cod": dico["cod"]}
     return dc
@@ -34,23 +61,17 @@ def meteo():
         dat = recup_data(ville)
         if dat["cod"] == '404':
             return redirect(url_for('index'))
-        v = dat["ville"]
-        t = dat["temp"]
-        h = dat["humidity"]
-        d = dat["description"].capitalize()
-        i = dat["icon"]
-
-        if t >= 25:
-            c = "red;"
-        elif t >= 20:
-            c = "orange;"
-        elif t <= 10:
-            c = "aqua;"
-        else:
-            c = "white;"
-        return render_template('meteo.html', ville=v, temp=t, humidity=h, description=d, icon=i, color=c)
+        return render_template(
+            'meteo.html',
+            ville=dat["ville"],
+            temp=dat["temp"],
+            humidity=dat["humidity"],
+            description=dat["description"].capitalize(),
+            icon=dat["icon"],
+            color=dat["color"],
+            country=dat["country"]
+        )
     return redirect(url_for('index'))
-
 
 @app.route('/meteo/<ville>')
 def meteoville(ville):
@@ -59,21 +80,17 @@ def meteoville(ville):
     dat = recup_data(ville)
     if dat["cod"] == '404':
         return redirect(url_for('index'))
-    v = dat["ville"]
-    t = dat["temp"]
-    h = dat["humidity"]
-    d = dat["description"].capitalize()
-    i = dat["icon"]
 
-    if t >= 25:
-        c = "red;"
-    elif t >= 20:
-        c = "orange;"
-    elif t <= 10:
-        c = "aqua;"
-    else:
-        c = "white;"
-    return render_template('meteo.html', ville=v, temp=t, humidity=h, description=d, icon=i, color=c)
+    return render_template(
+        'meteo.html',
+        ville=dat["ville"],
+        temp=dat["temp"],
+        humidity=dat["humidity"],
+        description=dat["description"].capitalize(),
+        icon=dat["icon"],
+        color=dat["color"],
+        country=dat["country"]
+    )
 
 
 if __name__ == '__main__':
